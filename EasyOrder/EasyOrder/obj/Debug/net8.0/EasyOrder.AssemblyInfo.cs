f1b68b350f613162f74b69c161e4e8ed2561b5b1
Flori <fv57184@ubt-uni.net>
2023-12-27 18:17:19 +0100

    commit

--- EasyOrder/EasyOrder/obj/Debug/net8.0/EasyOrder.AssemblyInfo.cs
+++ EasyOrder/EasyOrder/obj/Debug/net8.0/EasyOrder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyOrder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1bccbd260991ff86f90e3ed44b7e941d51a86be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyOrder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyOrder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
