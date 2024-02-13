34ad823434e5e46bd655d7ae184588be25de7800
ElonBytyqi <elon.bytyqi@hotmail.com>
2024-02-14 00:45:35 +0100

    waiter page

--- EasyOrder-React/EasyOrder/src/App.jsx
+++ EasyOrder-React/EasyOrder/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import {Routes,Route,Navigate} from "react-router-dom"
 // import Home from './Pages/Home'
 import {HomePage} from './Pages/HomePage'
+import CategoriesCard from './Pages/Components/CategoriesCard'
+import Orders from './Pages/Components/Orders'
 
 function App() {
   
@@ -11,6 +13,8 @@ function App() {
     <Routes>
       <Route index element={<Navigate to="/home" />}/>
       <Route path="/home" element={<HomePage />}/>
+      <Route path="/order" element={<Orders />}/>
+      
     </Routes>
     </>
   )
---
+++ EasyOrder-React/EasyOrder/src/Pages/Components/CategoriesCard.jsx
@@ -0,0 +1,17 @@
+
+import "../Components/CategoriesCard.css";
+import coffee from "../../assets/coffe.png";
+
+
+export default function CategoriesCard() {
+  return (
+    <div className='card'>
+      <img src="" alt="" />
+      <div className="titulli">
+      <h2> Breakfast</h2>
+      <p> 13 items</p>
+      </div>
+    </div>
+  )
+}
+
