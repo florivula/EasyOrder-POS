51090160196133b1e15191f3764182c741485a72
ElonBytyqi <elon.bytyqi@hotmail.com>
2024-02-22 15:27:49 +0100

    d

---
+++ EasyOrder/EasyOrder/Migrations/20240222131240_CategoryAndProducts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyOrder.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryAndProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Images",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Images",
+                table: "Products");
+        }
+    }
+}
---
+++ EasyOrder/EasyOrder/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyOrder.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public string Images { get; set; }
+    }
+}
